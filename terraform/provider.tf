51768b686585aa644cdfcdabf02334273923b993
Bapt <achbapt@gmail.com>
2025-03-21 11:44:49 +0000

    added provider HCP

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,3 +3,15 @@ provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
+
+# This file contains the provider configuration for Terraform Cloud, add it to your existing provider configuretion (dont replace)
+
+terraform {
+  cloud {
+    organization = "Limoncino-Corp"
+
+    workspaces {
+      name = "Limoncino"
+    }
+  }
+}
