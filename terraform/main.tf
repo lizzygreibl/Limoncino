d2f0ce33338a98f8d129a807e9d88eeef260555b
lizzygreibl <elisabeth.greibl@gmail.com>
2025-03-21 10:26:20 +0000

    changedmain

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,6 +35,23 @@ resource "azurerm_subnet" "example" {
 }
 
 
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example2" {
+  name                = "my-vnet2"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example2" {
+  name                 = "example2-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  
+}
+
 # Define the network interface
 
 resource "azurerm_network_interface" "example-nic" {
@@ -49,9 +66,50 @@ resource "azurerm_network_interface" "example-nic" {
   }
 }
 
+# Define the network interface
+
+resource "azurerm_network_interface" "example-nic2" {
+  name                = "example-nic2"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ipcfg2"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic2"
+  }
+}
+
 # Define the virtual machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "dtyryshkin-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id,
+  ]
+  size               = "Standard_DS1_v2"
+  admin_username     = "username"
+  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
+  disable_password_authentication = "false"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "dtyryshkin-vm2"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   network_interface_ids = [
