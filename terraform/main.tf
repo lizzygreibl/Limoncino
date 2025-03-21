fc3526ad91cfcf8782f31728c8d0b94445ee839e
lizzygreibl <elisabeth.greibl@gmail.com>
2025-03-21 10:05:39 +0000

    main and first provider

---
+++ terraform/main.tf
@@ -0,0 +1,76 @@
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "lizzyressource"
+  location = "West Europe"
+
+  tags = {
+    owner = "elisabeth.greibl@redbull.com"
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "baptressource"
+  location = "West Europe"
+
+  tags = {
+    owner = "Baptiste.Achard@redbull.com"
+  }
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "example" {
+  name                = "my-vnet"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  
+}
+
+
+# Define the network interface
+
+resource "azurerm_network_interface" "example-nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ipcfg"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "dtyryshkin-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id,
+  ]
+  size               = "Standard_DS1_v2"
+  admin_username     = "username"
+  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
+  disable_password_authentication = "false"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
